9d3518684b0c1746263e7e3abd623ce12782e606
tiroffp <tiroff.p@husky.neu.edu>
2016-08-05 16:06:39 -0400

    wording changes to more accurately reflect app usage

--- functional_tests/test_simple_roll_creation.py
+++ functional_tests/test_simple_roll_creation.py
@@ -6,15 +6,16 @@ from functional_tests.base import FunctionalTest
 class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_roll_and_retrieve_it_later(self):
-        # Mary has been losing track of all the things she needs to do
-        # and sees an ad for a to-do roll site. She goes to check out the
+        # Mary has been losing track of all the people she needs to keep track of
+        # and sees an ad for an attendance site. She goes to check out the
         # homepage
         self.browser.get(self.live_server_url)
 
-        # She notices that the title mentions to-do create_roll, so she
+        # She notices that the title mentions creating roll, so she
         # knows shes on the right site
         self.assertIn('Attendr', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Create", header_text)
         self.assertIn("Roll", header_text)
 
         # She is invited to make a roll right away
@@ -22,25 +23,25 @@ class NewVisitorTest(FunctionalTest):
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter an attendee')
 
-        # She types "Buy more artisanal cheeses"
-        inputbox.send_keys('Buy more artisanal cheeses')
+        # She types "Mary Man"
+        inputbox.send_keys('Mary Man')
 
         # When she hits enter, the page updates, and the page now
-        # rolls "1: Buy more artisanal cheeses" as an attendee on a to-do roll
+        # lists "1: Mary Man" as an attendee on a roll
         inputbox.send_keys(Keys.ENTER)
         mary_roll_url = self.browser.current_url
         self.assertRegex(mary_roll_url, '/create_roll/.+')
-        self.check_for_row_in_roll_table('1: Buy more artisanal cheeses')
+        self.check_for_row_in_roll_table('1: Mary Man')
 
         # There is still a textbox inviting here to enter another attendee
-        # she enters "Host a fancy party with fancy cheeses"
+        # she enters "David Dad"
         inputbox = self.get_attendee_input_box()
-        inputbox.send_keys('Host a fancy party with fancy cheeses')
+        inputbox.send_keys('David Dad')
         inputbox.send_keys(Keys.ENTER)
 
         # the page updates again, showing both her inputs
-        self.check_for_row_in_roll_table('1: Buy more artisanal cheeses')
-        self.check_for_row_in_roll_table('2: Host a fancy party with fancy cheeses')
+        self.check_for_row_in_roll_table('1: Mary Man')
+        self.check_for_row_in_roll_table('2: David Dad')
 
         # now a new user, Frank, comes along to the site
 
@@ -53,13 +54,11 @@ class NewVisitorTest(FunctionalTest):
         # Frank visits the home page. There is no sign of Mary's roll
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy more artisanal cheeses', page_text)
-        self.assertNotIn('Host a fancy party with fancy cheeses', page_text)
+        self.assertNotIn('Mary Man', page_text)
+        self.assertNotIn('David Dad', page_text)
 
-        # Frank starts a new roll by entering a new attendee. He is
-        # significantly less creative than Mary
-        inputbox = self.get_attendee_input_box()
-        inputbox.send_keys('Buy pork chops')
+        # Frank starts a new roll by entering a new attendee.
+        inputbox.send_keys('Frank Furt')
         inputbox.send_keys(Keys.ENTER)
 
         # Frank gets his own unique URL
@@ -69,7 +68,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Again, there is no trace of Mary's roll
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy more artisanal cheeses', page_text)
+        self.assertNotIn('Mary Man', page_text)
         self.assertIn('Buy pork chops', page_text)
 
         # Satisified, he leaves the page
