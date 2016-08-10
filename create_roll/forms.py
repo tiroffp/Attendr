ad03fe3b30a8980e02eff3e868f9ab92f48fc5e6
tiroffp <tiroff.p@husky.neu.edu>
2016-08-10 11:38:39 -0400

    added edit attendee form

--- create_roll/forms.py
+++ create_roll/forms.py
@@ -8,22 +8,22 @@ DUPLICATE_ATTENDEE_ERROR = "This name is already on the roll"
 
 class AttendeeForm(forms.models.ModelForm):
 
-        class Meta:
-                model = Attendee
-                fields = ('name',)
-                widgets = {
-                    'name': forms.fields.TextInput(attrs={
-                        'placeholder': 'Enter an attendee',
-                        'class': 'form-control input-lg',
-                        }),
-                }
-                error_messages = {
-                    'name': {'required': EMPTY_ERROR_MESSAGE}
-                }
+    class Meta:
+        model = Attendee
+        fields = ('name',)
+        widgets = {
+            'name': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter an attendee',
+                'class': 'form-control input-lg',
+                }),
+        }
+        error_messages = {
+            'name': {'required': EMPTY_ERROR_MESSAGE}
+        }
 
-        def save(self, for_roll):
-            self.instance.roll = for_roll
-            return super().save()
+    def save(self, for_roll):
+        self.instance.roll = for_roll
+        return super().save()
 
 
 class ExistingRollAttendeeForm(AttendeeForm):
@@ -40,3 +40,28 @@ class ExistingRollAttendeeForm(AttendeeForm):
 
     def save(self):
         return forms.models.ModelForm.save(self)
+
+
+class EditAttendeeForm(forms.models.ModelForm):
+    def __init__(self, attendee_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance = Attendee.objects.get(id=attendee_id)
+
+    class Meta:
+        model = Attendee
+        fields = ('name',)
+        widgets = {
+            'name': forms.fields.TextInput(attrs={
+                'class': 'form-control input-lg',
+                }),
+        }
+        error_messages = {
+            'name': {'required': EMPTY_ERROR_MESSAGE}
+        }
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'name': [DUPLICATE_ATTENDEE_ERROR]}
+            self._update_errors(e)
--- create_roll/tests/test_forms.py
+++ create_roll/tests/test_forms.py
@@ -3,7 +3,8 @@ from create_roll.forms import (
     AttendeeForm,
     EMPTY_ERROR_MESSAGE,
     DUPLICATE_ATTENDEE_ERROR,
-    ExistingRollAttendeeForm)
+    ExistingRollAttendeeForm,
+    EditAttendeeForm)
 from create_roll.models import Attendee, Roll
 
 
@@ -57,3 +58,40 @@ class ExistingRollAttendeeFormTest(TestCase):
         form = ExistingRollAttendeeForm(for_roll=roll, data={'name': 'hi'})
         new_attendee = form.save()
         self.assertEqual(new_attendee, Attendee.objects.all()[0])
+
+
+class EditAttendeeFormTest(TestCase):
+
+    def test_form_renders_attendee_name_input_with_attendee_name(self):
+        roll = Roll.objects.create()
+        attendee = Attendee.objects.create(roll=roll, name='Joe Biden')
+        form = EditAttendeeForm(initial={'name': attendee.name},
+                                attendee_id=attendee.id)
+        self.assertIn(attendee.name, form.as_p())
+
+    def test_form_validation_for_blank_attendees(self):
+        roll = Roll.objects.create()
+        attendee = Attendee.objects.create(roll=roll, name='John Madden')
+        form = EditAttendeeForm(attendee_id=attendee.id,
+                                data={'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'],
+                         [EMPTY_ERROR_MESSAGE])
+
+    def test_form_validation_for_duplicate_attendees(self):
+        roll = Roll.objects.create()
+        Attendee.objects.create(roll=roll, name='no twins!')
+        attendee = Attendee.objects.create(roll=roll, name='not a twin')
+        form = EditAttendeeForm(attendee_id=attendee.id,
+                                data={'name': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [DUPLICATE_ATTENDEE_ERROR])
+
+    def test_form_save(self):
+        roll = Roll.objects.create()
+        attendee = Attendee.objects.create(roll=roll, name='John Cene')
+        form = EditAttendeeForm(attendee_id=attendee.id,
+                                data={'name': 'John Cena'})
+        new_attendee = form.save()
+        self.assertEqual(new_attendee, Attendee.objects.all()[0])
+        self.assertNotEqual(new_attendee.name, attendee.name)
